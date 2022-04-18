470e19a12ff98de4b30790b308ef10fa48943c51
Gaurav Raj <techw803@gmail.com>
2022-04-19 03:49:51 +0530

    Added Cached Image support and scroll as well

---
+++ lib/models/article_model.dart
@@ -0,0 +1,13 @@
+class ArticleModel {
+  String title = '';
+  String description = '';
+  String url = '';
+  String imageUrl = '';
+
+  ArticleModel({
+    this.title = '',
+    this.description = '',
+    this.imageUrl = '',
+    this.url = '',
+  });
+}
